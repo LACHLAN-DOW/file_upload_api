b177976b45338afbbda0b5e6688cb5b2c97bf6ca
Lachlan Dow <lachlan.dow@gmail.com>
2025-01-26 14:03:20 +0000

    ADDITIONAL_FUNCTIONALITY - status tracking

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const multer = require("multer");
 const csv_parser = require("csv-parser");
 const app = express();
 const fs = require("fs");
+//const pLimit = require('p-limit');
 const upload = multer({ dest: "uploads/" });
 const port = process.env.PORT || 3000;
+const { v4: uuidv4 } = require("uuid"); // For generating unique upload IDs
 
+
+//const limit = pLimit(5);
+
+const taskStatusMap = new Map();
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
@@ -14,6 +20,14 @@ app.post("/upload", upload.single("file"), (req, res) => {
   if (!req.file) {
     return res.status(400).send("No File Found");
   }
+
+  const uploadId = uuidv4(); 
+  taskStatusMap.set(uploadId, { status: "processing", progress: 0 });
+  res.status(200).json({
+    uploadId,
+    message: "File uploaded successfully. Processing started.",
+  });
+
   const results = [];
   const validatePromises = [];
   let totalRecords = 0;
@@ -25,6 +39,9 @@ app.post("/upload", upload.single("file"), (req, res) => {
             if(!validation.valid){
                 results.push({name:data.name,email: data.email, error:'Invaid Email Format'});
             }
+            
+        const progress = Math.round((validatePromises.length / totalRecords) * 100);
+        taskStatusMap.set(uploadId, { status: "processing", progress });
         })
         validatePromises.push(validatePromise)
     })
@@ -36,11 +53,23 @@ app.post("/upload", upload.single("file"), (req, res) => {
             totalRecords: totalRecords,
             processedRecords: totalRecords - results.length,
             failedRecords: results.length,
-            details:[results]
+            details:results
         } 
-        res.status(200).json(summary); 
+        taskStatusMap.set(uploadId, { status: "completed", progress: 100, summary });
+
     });
 });
+
+app.get("/status/:uploadId", (req, res) => {
+    const { uploadId } = req.params;
+    const status = taskStatusMap.get(uploadId);
+  
+    if (!status) {
+      return res.status(404).json({ error: "Upload ID not found" });
+    }
+  
+    res.status(200).json(status);
+  });
 
 const mockValidateEmail = async (email) => {
   return new Promise((resolve, reject) => {
